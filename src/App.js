e3f24233ba70ec06022bc5f348bf6f10b05f79e8
Gustavo F. Silva <gustafsilva@hotmail.com>
2018-12-09 11:56:54 -0200

    Adicionando estado para texto e criando e mostrando seu inverso

--- src/App.js
+++ src/App.js
@@ -3,7 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    text: ''
+  }
+
+  updateText = (newText) => {
+    this.setState({
+      text: newText
+    });
+  }
+
   render() {
+    const { text } = this.state;
+    const reversedText = text.split('').reverse().join('');
+
     return (
       <div className="App">
         <header className="App-header">
@@ -11,8 +24,13 @@ class App extends Component {
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
         <div className="container">
-          <input type="text" placeholder="Say Something" />
-          <p className="echo">Echo:</p>
+          <input
+            type="text"
+            placeholder="Say Something"
+            value={text}
+            onChange={(event) => this.updateText(event.target.value)}
+          />
+          <p className="echo">Echo: {reversedText}</p>
           <p>This should mirror the text you typed into the input field.</p>
         </div>
       </div>
